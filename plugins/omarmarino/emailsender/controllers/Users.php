33f665618af28d3f96f97eeac7fd87c7ebe93b0a
Omar Yesid Mariño <omarinho@gmail.com>
2016-05-12 07:30:41 -0500

    Update Users.php

--- plugins/omarmarino/emailsender/controllers/Users.php
+++ plugins/omarmarino/emailsender/controllers/Users.php
@@ -53,8 +53,8 @@ class Users extends Controller
 
                   //Send the email
                   $url = 'https://api.sendgrid.com/';
-                  $user = 'omarinho';
-                  $pass = 'omar52hjb';
+                  $user = '*********';
+                  $pass = '*********';
 
                   $json_string = array(
                         'filters' => array('templates' => array('settings' => array('enable' => 1, 'template_id' => 'aee63675-ce8d-4c46-9948-93141e040b80'))),
